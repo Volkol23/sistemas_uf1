030ab2dcc227bb5781d4812c701ef5b89044d8e2
Adria <adriguiu97@gmail.com>
2020-11-16 10:00:14 +0100

    Clase de condiciones

--- condiciones.sh
+++ condiciones.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-cowsay "Ejercicio de condiciones"
+echo "COMPROBADOR DE EXISTENCIA DE ARCHIVOS V1.0"
+echo "------------------------------------------"
+echo ""
+echo "Indica el nombre de archivo a comprobar: "
 
-if [ "ola" == "ola" ]; then
-	echo "Es igual"
+read ARCHIVO
+
+if [ -e $ARCHIVO ]; then
+	cowsay "El archivo existe"
+else
+	echo "El archivo no existe"
 fi
