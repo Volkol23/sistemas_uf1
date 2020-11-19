04204c8330256e354c6feaa7b923e587cdc9fc96
Adria <adriguiu97@gmail.com>
2020-11-19 18:54:37 +0100

    Ejercicio bucles de colores

--- bucles.sh
+++ bucles.sh
@@ -2,11 +2,19 @@
 
 echo "Ejercicio de bulces"
 
+RED="[0;31"
+GREEN="[0;32"
+NOCOLOR="[0m"
 
 for FILE in `ls`; do
 	if [ -e $FILE ];then
-		echo "El archivo $FILE existe"
+		echo -e "\e$GREEN El archivo $FILE existe \e$NOCOLOR"
 	else
-		echo "El archivo $FILE no existe"
+		echo -e "\e$RED El archivo $FILE no existe \e$NOCOLOR"
 	fi	
+done
+
+
+for color in 0 1 2 3 4 5 6 7; do
+	echo -e "\e[0;3$color patata frita\e[0m"
 done
