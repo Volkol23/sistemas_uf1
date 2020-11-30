31572d00256b17392e52a1f7d241034c8a1660fb
Adria Sebastia (ENTI) <adriguiu97@gmail.com>
2020-11-30 11:23:55 +0100

    Ejercicio ffmpeg

--- md5mp4.sh
+++ md5mp4.sh
@@ -6,6 +6,8 @@ for FILE in `ls *.mp4`; do
 	echo "---------------------"
 	VIDEO=`file $FILE | grep -i media`
 	if [ "$VIDEO" != "" ];then
-		md5sum $FILE
+		#md5sum $FILE
+		FORMATFILE=`echo $FILE | cut "." -f 1`	
+		ffmpeg -i $FILE  $FORMATFILE.mkv
 	fi
 done
