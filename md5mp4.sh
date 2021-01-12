609cee4f4bcbcaa1fedfded4a67ced59a67064e5
Adria <adriguiu97@gmail.com>
2021-01-12 08:52:32 +0100

    Modificaiones md5mp4.sh

--- md5mp4.sh
+++ md5mp4.sh
@@ -2,12 +2,17 @@
 
 echo "Ejercicio de bulces MD5"
 
+EXTENSION="mp3"
+
 for FILE in `ls *.mp4`; do
 	echo "---------------------"
 	VIDEO=`file $FILE | grep -i media`
 	if [ "$VIDEO" != "" ];then
 		#md5sum $FILE
+		echo "Convirtiendo $FILE a $EXTENSION"
+		sleep 4
 		FORMATFILE=`echo $FILE | cut "." -f 1`	
-		ffmpeg -i $FILE  $FORMATFILE.mkv
+
+		ffmpeg -i $FILE  $FORMATFILE.$EXTENSION
 	fi
 done
